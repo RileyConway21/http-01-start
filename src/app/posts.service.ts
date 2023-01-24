1096f36ac9a9487c3e6db0108dc9def6d6421f1a
RileyConway21 <Rileyconway363@gmail.com>
2023-01-24 13:55:09 -0600

    Using a Service for Http Requests

---
+++ src/app/posts.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map } from 'rxjs/operators';
+
+import { Post } from './post.model';
+
+@Injectable({providedIn: "any"})
+export class PostsService {
+constructor(private http: HttpClient) {}
+
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = {title: title, content: content};
+        this.http
+        .post<{name: string}>(
+          'https://ng-complete-guide-947e0-default-rtdb.firebaseio.com/posts.json',
+          postData
+        )
+        .subscribe(responseData => {
+          console.log(responseData)
+        });    }
+
+
+    fetchPost () {
+        this.http.get<{[key: string]: Post }>('https://ng-complete-guide-947e0-default-rtdb.firebaseio.com/posts.json')
+        .pipe(map(responseData => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+        .subscribe(posts => {
+          
+        });
+    }
+}
