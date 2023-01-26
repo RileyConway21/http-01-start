cd00422c31f19a4ca47d1955b845127590d361c6
RileyConway21 <Rileyconway363@gmail.com>
2023-01-26 13:14:31 -0600

    Sending a DELETE Request

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -35,4 +35,8 @@ constructor(private http: HttpClient) {}
       );
         
     }
+
+    deletePosts() {
+      return this.http.delete('https://ng-complete-guide-947e0-default-rtdb.firebaseio.com/posts.json');
+    }
 }
