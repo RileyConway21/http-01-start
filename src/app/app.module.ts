b39ae9434490651fc951041a4575847a0184f0c7
RileyConway21 <Rileyconway363@gmail.com>
2023-02-06 13:58:08 -0600

    Multiple Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuthInterceptorService } from './auth-inceptor.server';
+import { LoggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +15,12 @@ import { AuthInterceptorService } from './auth-inceptor.server';
       provide: HTTP_INTERCEPTORS,
        useClass: AuthInterceptorService,
         multi: true
-      }
+      },
+      {
+        provide: HTTP_INTERCEPTORS,
+         useClass: LoggingInterceptorService,
+          multi: true
+        }
     ],
   bootstrap: [AppComponent]
 })
